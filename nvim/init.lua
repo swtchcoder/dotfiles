fb59fb94e454f40e7eab7f5a5b6c698079e9be93
swtchcoder <switchcodeur@switchcodeur.com>
2025-10-21 22:09:49 +0200

    Set tabstop and shiftwidth to 4 spaces

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,8 +20,8 @@ require("lazy").setup({
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
@@ -33,4 +33,4 @@ vim.opt.showmode = false
 vim.opt.shortmess:append("c")
 vim.opt.laststatus = 3
 vim.opt.clipboard = "unnamedplus"
-
+vim.opt.colorcolumn = "79"
